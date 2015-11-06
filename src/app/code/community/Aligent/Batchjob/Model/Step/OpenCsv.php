b2e3e998b52a925cf79c483c411dd2dde89a5cb6
Jim O'Halloran <jim@aligent.com.au>
2015-11-06 16:24:41 +1030

    Allow XML to define mode when  opening files.

--- src/app/code/community/Aligent/Batchjob/Model/Step/OpenCsv.php
+++ src/app/code/community/Aligent/Batchjob/Model/Step/OpenCsv.php
@@ -11,9 +11,18 @@ class Aligent_Batchjob_Model_Step_OpenCsv extends Aligent_Batchjob_Model_Step_Ab
         $vLocalFileName = $this->getParentJob()->getFilename();
         $this->getLogger()->log("Openning CSV file...".$vLocalFileName, Zend_Log::INFO);
 
-        $hCsv = fopen($vLocalFileName, 'r');
+        $hCsv = fopen($vLocalFileName, $this->_getFileMode());
         $this->getParentJob()->setFileHandle($hCsv);
         return true;
+    }
+
+
+    protected function _getFileMode() {
+        $vFileMode = $this->getParentJob()->getFileMode();
+        if ($vFileMode === null) {
+            $vFileMode = 'r';
+        }
+        return $vFileMode;
     }
 
 }
